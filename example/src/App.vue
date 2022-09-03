2d68a992d1df8b10e29dad22133668f15a0959af
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-03 12:59:57 +0530

    fix: example paremt component removed

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,23 +1,41 @@
 <template>
-	<parent />
+	<div :style="{ display: 'flex', justifyContent: 'center' }">
+		<div
+			:style="{
+				width: '400px',
+				margin: `10px auto`,
+				border: `1px solid #D1D5DB`,
+				padding: `25px 20px`,
+				borderRadius: 5,
+			}"
+		>
+			<authorizer-provider
+				:config="{
+					authorizerURL: 'http://localhost:8080',
+				}"
+				:onStateChangeCallback="stateChangeCallback"
+			>
+				<children />
+			</authorizer-provider>
+		</div>
+	</div>
 </template>
 
 <script>
-import Parent from './components/Parent.vue';
+import Children from './components/Children.vue';
 export default {
 	components: {
-		parent: Parent,
+		children: Children,
+	},
+	setup() {
+		const stateChangeCallback = (state) => {
+			console.log('state changes from client ==>> ', state);
+		};
+		return {
+			stateChangeCallback,
+		};
 	},
 };
 </script>
 
-<style scoped>
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-</style>
+<style scoped></style>
