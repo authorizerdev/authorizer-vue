1ef0afe9ec5d7175f36af57ed58d9fdd70f12888
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2022-09-05 10:05:16 +0530

    feat: add validate password util

--- src/utils/common.js
+++ src/utils/common.js
@@ -52,3 +52,55 @@ export const isValidOtp = (otp) => {
 export const formatErrorMessage = (message) => {
 	return message.replace(`[GraphQL] `, '');
 };
+
+export const validatePassword = (value) => {
+	const res = {
+		score: 0,
+		strength: '',
+		hasSixChar: false,
+		hasLowerCase: false,
+		hasUpperCase: false,
+		hasNumericChar: false,
+		hasSpecialChar: false,
+		maxThirtySixChar: false,
+	};
+
+	if (value.length >= 6) {
+		res.score = res.score + 1;
+		res.hasSixChar = true;
+	}
+
+	if (value.length > 0 && value.length <= 36) {
+		res.score = res.score + 1;
+		res.maxThirtySixChar = true;
+	}
+
+	Array.from(value).forEach((char) => {
+		if (char >= 'A' && char <= 'Z' && !res.hasUpperCase) {
+			res.score = res.score + 1;
+			res.hasUpperCase = true;
+		} else if (char >= 'a' && char <= 'z' && !res.hasLowerCase) {
+			res.score = res.score + 1;
+			res.hasLowerCase = true;
+		} else if (char >= '0' && char <= '9' && !res.hasNumericChar) {
+			res.score = res.score + 1;
+			res.hasNumericChar = true;
+		} else if (!res.hasSpecialChar) {
+			res.score = res.score + 1;
+			res.hasSpecialChar = true;
+		}
+	});
+
+	if (res.score <= 2) {
+		res.strength = 'Weak';
+	} else if (res.score <= 4) {
+		res.strength = 'Good';
+	} else if (res.score <= 5) {
+		res.strength = 'Strong';
+	} else {
+		res.strength = 'Very Strong';
+	}
+
+	const isValid = Object.values(res).every((i) => Boolean(i));
+	return { ...res, isValid };
+};
