b1d93599bdd3ccaa7c698303d57b50f160edcd8b
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2023-01-14 21:08:30 +0530

    fix: styled button

--- src/styledComponents/StyledButton.vue
+++ src/styledComponents/StyledButton.vue
@@ -1,5 +1,11 @@
 <template>
-	<button :type="type" :disabled="disabled" :onClick="onClick">
+	<button
+		class="styled-button"
+		:style="style"
+		:type="type"
+		:disabled="disabled"
+		:onClick="onClick"
+	>
 		<slot></slot>
 	</button>
 </template>
@@ -11,7 +17,22 @@ export default {
 	name: 'StyledButton',
 	props: ['style', 'type', 'appearance', 'disabled', 'onClick'],
 	setup(props) {
-		return { ...props, ButtonAppearance };
+		return {
+			...props,
+			style: {
+				width: '100%',
+				backgroundColor: props.disabled
+					? 'var(--authorizer-primary-disabled-color)'
+					: props.appearance === ButtonAppearance.Primary
+					? 'var(--authorizer-primary-color)'
+					: 'var(--authorizer-white-color)',
+				color:
+					props.appearance === ButtonAppearance.Default
+						? 'var(--authorizer-text-color)'
+						: 'var(--authorizer-white-color)',
+				border: props.appearance === ButtonAppearance.Primary ? '0px' : '1px',
+			},
+		};
 	},
 };
 </script>
